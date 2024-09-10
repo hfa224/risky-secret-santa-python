e2c5a370f8b9317c44d28400733fd0f7f311f27b
hfa224 <helenffionadams@gmail.com>
2024-09-10 17:51:16 +0100

    refs #5 update home page test

--- tests/test_home.py
+++ tests/test_home.py
@@ -1,12 +1,16 @@
+""" Test the index/home page """
 import pytest
 from secret_santa.db import get_db
 
 
 def test_index(client, auth):
+    """ Test that the index page displays correct actions for
+        logged in and logged out users                       """
     response = client.get('/')
-    assert b"Log In" in response.data
+    assert b"Log in" in response.data
     assert b"Register" in response.data
 
     auth.login()
     response = client.get('/')
+    print(response.data)
     assert b'Log Out' in response.data
