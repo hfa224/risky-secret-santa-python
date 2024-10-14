4709537e4a030e19ed8144d1b3ea6ac3e4e8cd33
hfa224 <helenffionadams@gmail.com>
2024-10-14 20:01:51 +0100

    Update tests

--- tests/event_data.sql
+++ tests/event_data.sql
@@ -1,4 +1,4 @@
-INSERT INTO event (event_title, draw_date, event_date, event_description, cost)
+INSERT INTO event (user_id, event_title, draw_date, event_date, event_description, cost)
 VALUES
-  ('test title', '2018-01-01', '2018-01-01', 'my event description', 'ten english pounds'),
-  ('test title 2024', '2024-01-01', '2024-01-01', 'the recent event', 'twenty english pounds');
+  ('event_owner', 'test title', '2018-01-01', '2018-01-01', 'my event description', 'ten english pounds'),
+  ('event_owner', 'test title 2024', '2024-01-01', '2024-01-01', 'the recent event', 'twenty english pounds');
--- tests/test_db.py
+++ tests/test_db.py
@@ -26,13 +26,20 @@ def test_init_db_command(runner, monkeypatch):
     class Recorder:
         """Recorder class"""
 
-        called = False
+        called_init_db = False
+        called_init_user = False
 
     def fake_init_db():
-        """return fake db"""
-        Recorder.called = True
+        """fake init db method"""
+        Recorder.called_init_db = True
+
+    def fake_add_initial_user():
+        """fake add intial user method"""
+        Recorder.called_init_user = True
 
     monkeypatch.setattr("secret_santa.db.init_db", fake_init_db)
+    monkeypatch.setattr("secret_santa.db.add_initial_user", fake_add_initial_user)
     result = runner.invoke(args=["init-db"])
     assert "Initialized" in result.output
-    assert Recorder.called
+    assert Recorder.called_init_db
+    assert Recorder.called_init_user
